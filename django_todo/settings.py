9e9ae1d8edfaadb42c4926447ea6ef70a605d3d6
Blaize Egelhof <Blaize.Egelhof@outlook.com>
2023-10-10 14:34:57 +0000

    Fixed allowed_hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-%-9op)-91_(p=7o=slyrzt6&#hzpw6w3su^%t*kt-6=ouh$%&h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-blaizeegelh-hellodjango-pnpfjsidb4n.ws-eu105.gitpod.io']
+ALLOWED_HOSTS = ['django-todo-app-blaize-egelhof-293746dfdc7f.herokuapp.com']
 
 
 # Application definition
